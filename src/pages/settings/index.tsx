7dc03df6c7ad108703de17dd97e0f5f5c1a497e5
Angelo Menti <angelo.omarzinho@outlook.com>
2022-10-29 18:23:04 -0300

    pages - settings created

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,10 +1,139 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { Linking, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ThemeContext } from 'styled-components/native';
+import { Header } from '../../components/header';
+import { ThemeModel } from '../../styles/themes/types';
+import { Background, Head, Title, Content, IconView, BackgroundIcon, IconMaterial, IconFontAwesome, ContentModal, ButtonGroup } from './styles';
+import IconAntDesign from 'react-native-vector-icons/AntDesign';
+import { Button } from '../../components/button';
+import { useNavigation } from '@react-navigation/native';
+import { Modal } from '../../components/modal';
+import { useDispatch } from 'react-redux';
+import { clearState } from '../../store/user/slice';
 
-// import { Container } from './styles';
+export const Settings: React.FC = () => {
+  const themeContext = useContext<ThemeModel>(ThemeContext)
+  const { goBack } = useNavigation()
+  const [showModal, setShowModal] = useState(false)
+  const dispatch = useDispatch()
+  
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: themeContext.background }}>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <ContentModal>
+          <IconFontAwesome 
+            name='inbox'
+            color={themeContext.primary}
+            size={40}/>
 
-const Settings: React.FC = () => {
-  return <View />;
+          <Title 
+            size={18}
+            color={themeContext.text}
+            weight={700}>
+            Are you sure?
+          </Title>
+
+          <Title
+            size={14}
+            color={themeContext.secundaryText}
+            weight={400}>
+              You will remove all saved information and will not be able to recover it
+          </Title>
+          <ButtonGroup>
+            <Button
+              color={themeContext.text}
+              title='Cancel'
+              type='clear'
+              onPress={() => setShowModal(false)}/>
+            
+            <Button
+              title='Remove'
+              type='solid'
+              onPress={() => dispatch(clearState())}/>
+          </ButtonGroup>
+        </ContentModal>
+      </Modal>
+
+      <BackgroundIcon>
+        <Button
+          onPress={() => goBack()}
+          type={'clear'}
+          title={
+            <IconAntDesign
+              name='left'
+              size={25}
+              color={themeContext.primary} />
+          } />
+      </BackgroundIcon>
+
+      <Background>
+        <Header
+          title='My'
+          titleBold='configurations!'
+        />
+
+        <Head>
+          <Title
+            color={themeContext.secundaryText}>
+            CONTENT
+          </Title>
+        </Head>
+
+        <Content onPress={() => setShowModal(true)}>
+          <IconView>
+            <IconMaterial
+              name='save-alt'
+              color={themeContext.icon}
+              size={30}/>
+            <Title color={themeContext.text}>Clear Data</Title>
+          </IconView>
+
+          <IconAntDesign
+            name='right'
+            color={themeContext.primary}
+            size={18} />
+        </Content>
+
+        <Head>
+          <Title
+            color={themeContext.secundaryText}>
+            ABOUT
+          </Title>
+        </Head>
+
+        <Content onPress={() => Linking.openURL('https://github.com/eumaninho54/HelpDogs')}>
+          <IconView>
+            <IconFontAwesome
+              name='github'
+              color={themeContext.icon}
+              size={30}/>
+
+            <Title color={themeContext.text}>See the source</Title>
+          </IconView>
+
+          <IconAntDesign
+            name='right'
+            color={themeContext.primary}
+            size={18} />
+        </Content>
+
+        <Content onPress={() => Linking.openURL('https://www.linkedin.com/in/angelo-menti-663040210/')}>
+          <IconView>
+            <IconFontAwesome
+              name='linkedin'
+              color={themeContext.icon}
+              size={30}/>
+
+            <Title color={themeContext.text}>Follow me on</Title>
+          </IconView>
+
+          <IconAntDesign
+            name='right'
+            color={themeContext.primary}
+            size={18} />
+        </Content>
+      </Background>
+    </SafeAreaView>
+  )
 }
-
-export default Settings;
--- src/pages/settings/styles.ts
+++ src/pages/settings/styles.ts
@@ -1,1 +1,89 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
+import {ThemeModel} from '../../styles/themes/types';
+import IconMaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import IconAwesome from 'react-native-vector-icons/FontAwesome';
+
+
+interface props {
+  theme: ThemeModel;
+  color: string;
+  weight: number;
+  size?: number;
+}
+
+export const Background = styled.View`
+  width: 100%;
+  height: 100%;
+  background-color: ${({theme}: props) => theme.background};
+`;
+
+export const BackgroundIcon = styled.View`
+  margin: 10px;
+  width: 40px;
+  height: 40px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Head = styled.View.attrs(({theme}: props) => ({
+  shadowColor: theme.shadow,
+  shadowOffset: {
+    width: 0,
+    height: 4,
+  },
+  shadowOpacity: 0.22,
+  shadowRadius: 5.46,
+  elevation: 9,
+}))`
+  width: auto;
+  margin: 20px 20px 10px;
+  background-color: ${({theme}: props) => theme.secundaryBackground};
+  justify-content: center;
+  padding: 13px 20px;
+`;
+
+
+export const Content = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  padding: 10px 10px;
+  margin: 10px 20px;
+  width: auto;
+`
+
+export const IconView = styled.View`
+  flex-direction: row;
+  align-items: center;
+  width: 80%;
+`
+
+export const Title = styled.Text`
+  font-size: ${({size}: props) => `${size || 16}px`};
+  color: ${({color}: props) => color};
+  font-weight: ${({weight}: props) => weight || 400};
+`;
+
+export const IconMaterial = styled(IconMaterialIcons)`
+  min-width: 30px;
+  margin-right: 10px;
+`
+
+export const IconFontAwesome = styled(IconAwesome)`
+  min-width: 30px;
+  margin-right: 10px;
+`
+
+export const ContentModal = styled.View`
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-between;
+`
+
+export const ButtonGroup = styled.View`
+  width: auto;
+  margin-top: 20px;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+`
